d80c02f8aae033e6a1eaf0b3d6643238d6c0cb54
Jack Boeri <53635108+JekBurri@users.noreply.github.com>
2023-01-30 21:12:36 -0800

    have to push again for some reason

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const database = include('databaseConnection');
 const dbModel = include('databaseAccessLayer');
 // const dbModel = include('staticData');
+//
 
 router.get('/', async (req, res) => {
 	console.log("page hit");
